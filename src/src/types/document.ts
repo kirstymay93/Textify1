abe1f3d443317632042684c6837d75e86514fa83
kirstymay93 <ndiscampaign@gmail.com>
2026-07-03 17:49:03 +1000

    Update document.ts

--- src/src/types/document.ts
+++ src/src/types/document.ts
@@ -1,12 +1,4 @@
 export type Block = {
   id: string;
-  type: "text";
   content: string;
-};
-
-export type Document = {
-  id: string;
-  title: string;
-  blocks: Block[];
-  updatedAt: number;
 };
