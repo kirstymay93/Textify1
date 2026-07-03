9ac4d292f69ad6758a154e8e5356213267a0e21e
kirstymay93 <ndiscampaign@gmail.com>
2026-07-04 00:56:13 +1000

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/index.ts',
-    '!src/main.tsx',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+
+  roots: ["<rootDir>/src"],
+
+  testMatch: [
+    "**/__tests__/**/*.(ts|tsx)",
+    "**/?(*.)+(spec|test).(ts|tsx)"
   ],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/main.tsx"
+  ]
 };
