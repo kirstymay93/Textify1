ccecf28983e875ec8dff6be7223b907ba318d5fc
kirstymay93 <ndiscampaign@gmail.com>
2026-07-04 00:37:48 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Router } from 'wouter';
-import App from './App';
-import '../index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Router } from "wouter";
+import App from "./App";
+import "../index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <Router>
       <App />
